b888e9fc6cd2b91d2830e398afb7bf80d5922a41
rbhatt199924 <rbhatt199924@gmail.com>
2023-03-08 20:29:15 +0530

    add app class and main file

--- main.rb
+++ main.rb
@@ -1,10 +1,39 @@
-require './person'
-require './trimmer_decorator'
-require './capitalize_decorator'
+require_relative 'app'
 
-person = Person.new(22, 'maximilianus')
-p person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-p capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-p capitalized_trimmed_person.correct_name
+def main
+  app = App.new
+  app.start_console
+end
+
+def list_of_options
+  puts
+  puts 'Please choose an option by entering a number:'
+  puts '1 - list all books'
+  puts '2 - list all people'
+  puts '3 - create a person'
+  puts '4 - create a book'
+  puts '5 - create a renter'
+  puts '6 - list all renters for a given person id'
+  puts '7 - Exit'
+end
+
+def option(input)
+  case input
+  when '1'
+    list_all_books
+  when '2'
+    list_all_persons
+  when '3'
+    create_person
+  when '4'
+    create_book
+  when '5'
+    create_rental
+  when '6'
+    list_all_rentals
+  else
+    puts 'Please enter a number between 1 and 7'
+  end
+end
+
+main
