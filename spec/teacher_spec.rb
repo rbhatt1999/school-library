242ee9df1a1b27798a7132fe312cd715e3a83483
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-03-15 16:32:36 +0500

    Update teacher_spec.rb
    In this commit
    - Modify a unit test case.

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -3,8 +3,9 @@ require_relative '../teacher'
 describe Teacher do
   context 'For the class Teacher' do
     teacher = Teacher.new('Power', 31, 'Lucas')
-    it 'creates a new teacher with the name "Lucas"' do
+    it 'creates a new teacher with the name "Lucas and age 31"' do
       expect(teacher.name).to eql "Lucas"
+      expect(teacher.age).to eql 31
     end
 
     it 'should have always permission to use services' do
