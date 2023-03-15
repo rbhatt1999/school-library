696b3b1644c95fa99272e9c32d6834c2955f10e9
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-03-15 16:05:27 +0500

    Update person_spec.rb
    In this commit
    - Add another unit test for the 'add rental' method of Person class.

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,7 +1,8 @@
 require_relative '../person'
+require_relative '../rental'
 
 describe Person do
-  context 'When a new person is created' do
+  context 'For the class Person' do
     person = Person.new(age: 25, name: 'Rohit', parent_permission: true)
     another_person = Person.new(age: 15, name: 'Jitendar', parent_permission: false)
     it 'should return true if he can use service for a person wtih age greater than 18' do
@@ -10,6 +11,12 @@ describe Person do
 
     it 'should return false if he can use service for a person with age less than 18' do
       expect(another_person.can_use_services?).to eq false
+    end
+
+    it 'should add into rentals for a new book' do
+      book = Book.new('Why Nations Fail', 'James')
+      person.add_rental(book, '12/12/1992')
+      expect(person.rentals.length).to eq 1
     end
 
   end
