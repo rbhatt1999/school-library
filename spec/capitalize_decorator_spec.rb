8e4bed1752f48663d2e9280f0bc7a0afe5d8c5bb
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-03-15 18:14:09 +0500

    Create capitalize_decorator_spec.rb
    In this commit
    - Add unit tests for the methods of "CapitalizeDecorator" class.

---
+++ spec/capitalize_decorator_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../capitalize_decorator'
+require_relative '../person'
+
+describe CapitalizeDecorator do
+  before(:all) do
+    @person = Person.new(age: 26, name:'obama')
+    @capitalize = CapitalizeDecorator.new(@person)
+  end
+
+  describe 'For the CapitalizeDecorator class' do
+    it 'returns the given name "obama" with the capitalized first letter' do
+      expect(@capitalize.correct_name).to eql 'Obama'
+    end
+  end
+end
